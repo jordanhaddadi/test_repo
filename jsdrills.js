1b732788d0a07e7ff9306b55cef24270fcd38242
Ethan Frigon <ethan.frigon@gmail.com>
2018-10-29 11:19:35 -0700

    added try catch

--- jsdrills.js
+++ jsdrills.js
@@ -1,13 +1,20 @@
 'use strict';
 
 function getYearOfBirth(age) {
-    return 2018 - age;
+  if (age < 0){
+    throw new Error('Age cannot be negative');
+  }
+  return 2018 - age;
 }
 
 function createGreeting(name, age) {
-    const yearOfBirth = 2018 - age;
-    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+  const yearOfBirth = 2018 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
+
+try {
+  const greeting1 = createGreeting('jordan', 27);
+} catch console.error(e.message);
 
 const greeting1 = createGreeting('jordan', 27);
 console.log(greeting1);

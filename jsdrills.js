f715674b5e2d9d38f9409cec093a2c73d62bae22
Ethan Frigon <ethan.frigon@gmail.com>
2018-10-29 11:03:26 -0700

    changed fn to use parameters

--- jsdrills.js
+++ jsdrills.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function createGreeting() {
-  return 'Hi, my name is Jordan and I\'m 27 years old';
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('jordan', 27);
 console.log(greeting1);

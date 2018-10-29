ca058ee8ec78513436c2c9f2cedf6f7df46441da
Jordan Haddadi <jordanhaddadi@gmail.com>
2018-10-29 14:12:59 -0400

    created a new function for year of birth

--- jsdrills.js
+++ jsdrills.js
@@ -1,7 +1,12 @@
 'use strict';
 
+function getYearOfBirth(age) {
+    return 2018 - age;
+}
+
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2018 - age;
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting('jordan', 27);
